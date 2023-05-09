1543e452399d4b699b933778d2e84c47cbb4e808
Erick-Mtz <88734208+Ing-Erick-Mtz-Electronico@users.noreply.github.com>
2023-05-09 17:26:49 -0500

    creacion de componentes

---
+++ src/app/components/catalog/catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+
+}
